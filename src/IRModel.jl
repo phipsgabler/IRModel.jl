708e67e23f09361272f0f52f29c94688c048161a
Philipp Gabler <phipsgabler@users.noreply.github.com>
2022-08-17 17:15:22 +0200

    Add conversion to AST

--- src/IRModel.jl
+++ src/IRModel.jl
@@ -10,6 +10,30 @@ function __tilde__ end
 function __vn__ end
 
 
+function transformed_ir(mod, body)
+    newbody = MT.postwalk(body) do x
+        if MT.@capture(x, {vn_})
+            return Expr(:call, __vn__, QuoteNode(AbstractPPL.varname(vn)))
+        else
+            return x
+        end
+    end
+    
+    ir = only(Meta.lower(mod, newbody).args)
+    map!(ir.code, ir.code) do line
+        if Meta.isexpr(line, :call, 2) && line.args[1] == __vn__
+            return Expr(:braces, AbstractPPL.drop_escape(line.args[2].value))
+        elseif Meta.isexpr(line, :call, 2) && line.args[1] == :(~)
+            return Expr(:(~), line.args[2:end]...)
+        else
+            return line
+        end
+    end
+
+    return ir
+end
+
+
 function irmodel(mod, expr)
     fundef = MT.splitdef(expr)
     name = fundef[:name]
@@ -19,70 +43,68 @@ function irmodel(mod, expr)
     whereparams = fundef[:whereparams]
     body = fundef[:body]
 
-    newbody = MT.postwalk(body) do x
-        if MT.@capture(x, ~(lhs_, rhs_))
-            return Expr(:call, __tilde__, lhs, rhs)
-        elseif MT.@capture(x, {vn_})
-            return Expr(:call, __vn__, QuoteNode(AbstractPPL.varname(vn)))
-        else
-            return x
-        end
-    end
-
-    ir = only(Meta.lower(mod, newbody).args)
-    map!(ir.code, ir.code) do line
-        if Meta.isexpr(line, :call, 3) && line.args[1] == __tilde__
-            # return line
-            return Expr(:call, :~, line.args[2:3]...)
-        elseif Meta.isexpr(line, :call, 2) && line.args[1] == __vn__
-            return Expr(:braces, AbstractPPL.drop_escape(line.args[2].value))
-        else
-            return line
-        end
-    end
+    ir = transformed_ir(mod, body)
 
     resolve(expr::Expr) = Expr(expr.head, resolve.(expr.args)...)
     resolve(s::Core.SlotNumber) = ir.slotnames[s.id]
-    resolve(v::Core.SSAValue) = Symbol(v.id)
+    resolve(v::Core.SSAValue) = v #Symbol("%", v.id)
     resolve(x) = x
-    jumplabel(i) = Meta.quot(Symbol("#", i))
-    
-    expr_lines = []
-    jump_targets = Set{Int}()
+    jumplabel(i) = Symbol("#", i)
 
-    function push_line!(i, expr)
-        if i in jump_targets
-            return push!(expr_lines, :(@label $(jumplabel(i)) $expr))
-        else
-            return push!(expr_lines, expr)
+    blocks = Expr[]
+    function push_line!(block, expr)
+        if length(blocks) < block
+            push!(blocks, Expr(:block, :(@label $(jumplabel(length(blocks) + 1)))))
         end
+        
+        push!(blocks[end].args, expr)
     end
 
+    # add all lines to blocks, but leaving control flow statements intact
     current_block = 1
     for i in eachindex(ir.code)
         line = ir.code[i]
-        if line isa Core.GotoNode
-            push!(jump_targets, line.label)
-            push_line!(i, :(@goto $(jumplabel(line.label))))
-            current_block += 1
-        elseif line isa Core.GotoIfNot
-            push!(jump_targets, line.dest)
-            push_line!(line, :($(line.cond) || @goto $(jumplabel(line.dest))))
-            current_block += 1
-        elseif line isa Core.ReturnNode
-            push_line!(i, :(return $(line.val)))
+        if line isa Core.GotoNode || line isa Core.GotoIfNot || line isa Core.ReturnNode
+            push_line!(current_block, line)
             current_block += 1
         elseif Meta.isexpr(line, :(=))
-            push_line!(i, Expr(:(=), resolve(line.args[1]), resolve.(line.args[2:end])...))
+            push_line!(current_block, Expr(:(=), resolve(line.args[1]), resolve.(line.args[2:end])...))
         else
-            push_line!(i, :($(Symbol("%", i)) = $(resolve(line))))
+            # push_line!(current_block, :($(Symbol("%", i)) = $(resolve(line))))
+            push_line!(current_block, :($(Core.SSAValue(i)) = $(resolve(line))))
         end
     end
 
-    code = MT.striplines(Expr(:block, expr_lines...))
+    # figure out block ranges
+    block_ranges = UnitRange{Int}[]
+    previous_block_end = 0
+    for block in blocks
+        block_end = previous_block_end + length(block.args) - 1
+        push!(block_ranges, (previous_block_end + 1):block_end)
+        previous_block_end = block_end
+    end
 
-    # return (;ir, code, args)
-    return QuoteNode(code)
+    # replace control flow statements using block numbers
+    line2block(i) = findfirst(u -> i ∈ u, block_ranges)
+    for block in blocks
+        for i in eachindex(block.args)
+            line = block.args[i]
+            if line isa Core.GotoNode
+                block.args[i] = :(@goto $(jumplabel(line2block(line.label))))
+            elseif line isa Core.GotoIfNot
+                block.args[i] = :($(resolve(line.cond)) || @goto $(jumplabel(line2block(line.dest))))
+            elseif line isa Core.ReturnNode
+                block.args[i] = :(return $(line.val))
+            end
+        end
+    end
+
+    # reinsert line numbers
+    code = MT.striplines(Expr(:block, blocks...))
+    # TODO: linetable, codelocs
+
+    return (;ir, code, args)
+    # return QuoteNode(MT.identity(code))
 end
 
 macro irmodel(expr)
